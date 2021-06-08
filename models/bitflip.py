1d7a434463ea3c87b5ab9966b0255ed6d2dd3713
chloewin <chloewin@uw.edu>
2021-06-07 20:45:09 -0700

    update for server

--- models/bitflip.py
+++ models/bitflip.py
@@ -36,103 +36,109 @@ There are other specifications including amount of time, number of epochs, learn
 """
 
 def main():
-	main_name = "3bit"#"brnn200_noncued_moreascs_diffinit"#"brnn200_sussillo8_batched_hisgmav_predrive_scaleasc_wtonly_agn_nodivstart"#lng_lngersim_uniformoffset_furthertrain"
-	base_name = "figures_wkof_053021/" + main_name
-	base_name_save = "traininfo_wkof_053021/" + main_name
-	base_name_model = "models_wkof_053021/" + main_name
+        main_name = "3bit_longer"#"brnn200_noncued_moreascs_diffinit"#"brnn200_sussillo8_batched_hisgmav_predrive_scaleasc_wtonly_agn_nodivstart"#lng_lngersim_uniformoffset_furthertrain"
+        on_server = True
+        if on_server:
+            base_name = main_name
+            base_name_save = main_name
+            base_name_model = main_name
+        else: 
+            base_name = "figures_wkof_053021/" + main_name
+            base_name_save = "traininfo_wkof_053021/" + main_name
+            base_name_model = "models_wkof_053021/" + main_name
 
-	use_rnn = True
-	hid_size = 64
-	input_size = 3
-	output_size = 3
+        use_rnn = True
+        hid_size = 64
+        input_size = 3
+        output_size = 3
 
-	# Generate data
-	sim_time = 2056 * 0.05
-	dt = 0.05
-	
-	batch_size = 256
+        # Generate data
+        sim_time = 2056 * 0.05
+        dt = 0.05
+        
+        batch_size = 256
 
-	traindataset = ut.ThreeBitDataset(int(sim_time / dt), dataset_length=2048)
+        traindataset = ut.ThreeBitDataset(int(sim_time / dt), dataset_length=2048)
 
-	# # Generate model
-	# delay = int(0.5 / dt)
-	if use_rnn:
-		model = RNNFC(in_size = input_size, hid_size = hid_size, out_size = output_size)
-	else:
-		model = BNNFC(in_size = input_size, hid_size = hid_size, out_size = output_size)
-	# model.load_state_dict(torch.load("trained_model.pt"))#"saved_models/models_wkof_051621/brnn200_sussillo8_batched_hisgmav_predrive_scaleasc_wtonly_agn_nodivstart.pt"))
-	# Train model
-	num_epochs = 20
-	lr = 0.0025
+        # # Generate model
+        # delay = int(0.5 / dt)
+        if use_rnn:
+                model = RNNFC(in_size = input_size, hid_size = hid_size, out_size = output_size)
+        else:
+                model = BNNFC(in_size = input_size, hid_size = hid_size, out_size = output_size)
+        # model.load_state_dict(torch.load("trained_model.pt"))#"saved_models/models_wkof_051621/brnn200_sussillo8_batched_hisgmav_predrive_scaleasc_wtonly_agn_nodivstart.pt"))
+        # Train model
+        num_epochs = 2000
+        lr = 0.0025
 
-	# num_epochss = [200,100,50,10,1,1]
-	training_info = ut.train_rbnn(model, traindataset, batch_size, num_epochs, lr, reg_lambda=0, glifr = not use_rnn)
+        # num_epochss = [200,100,50,10,1,1]
+        training_info = ut.train_rbnn(model, traindataset, batch_size, num_epochs, lr, reg_lambda=0, glifr = not use_rnn)
 
-	torch.save(model.state_dict(), "saved_models/" + base_name_model + ".pt")
+        torch.save(model.state_dict(), "saved_models/" + base_name_model + ".pt")
 
-	colors = ["sienna", "peru", "peachpuff", "salmon", "red", "darkorange", "purple", "fuchsia", "plum", "darkorchid", "slateblue", "mediumblue", "cornflowerblue", "skyblue", "aqua", "aquamarine", "springgreen", "green", "lightgreen"]
+        colors = ["sienna", "peru", "peachpuff", "salmon", "red", "darkorange", "purple", "fuchsia", "plum", "darkorchid", "slateblue", "mediumblue", "cornflowerblue", "skyblue", "aqua", "aquamarine", "springgreen", "green", "lightgreen"]
 
-	# Plot outputs
+        # Plot outputs
 
-	ut.plot_predictions(model, int(sim_time / dt), batch_size)
-	plt.savefig("figures/" + base_name + "_final_outputs")
-	plt.close()
+        ut.plot_predictions(model, int(sim_time / dt), batch_size)
+        plt.savefig("figures/" + base_name + "_final_outputs")
+        plt.close()
 
-	# Plot losses
-	plt.plot(training_info["losses"])
-	plt.xlabel("epoch #")
-	plt.ylabel("loss")
-	plt.savefig("figures/" + base_name + "_losses")
-	plt.close()
-	torch.save(training_info["losses"], "traininfo/" + base_name_save + "_losses.pt")
+        # Plot losses
+        plt.plot(training_info["losses"])
+        plt.xlabel("epoch #")
+        plt.ylabel("loss")
+        plt.savefig("figures/" + base_name + "_losses")
+        plt.close()
+        torch.save(training_info["losses"], "traininfo/" + base_name_save + "_losses.pt")
 
-	colors = ["sienna", "gold", "chartreuse", "darkgreen", "lightseagreen", "deepskyblue", "blue", "darkorchid", "plum", "darkorange", "fuchsia", "tomato", "cyan", "greenyellow", "cornflowerblue", "limegreen", "springgreen", "green", "lightgreen", "aquamarine", "springgreen", "green", "lightgreen", "aquamarine", "springgreen", "green", "lightgreen", "aquamarine", "springgreen", "green", "lightgreen", "aquamarine", "springgreen", "green", "lightgreen"]
+        colors = ["sienna", "gold", "chartreuse", "darkgreen", "lightseagreen", "deepskyblue", "blue", "darkorchid", "plum", "darkorange", "fuchsia", "tomato", "cyan", "greenyellow", "cornflowerblue", "limegreen", "springgreen", "green", "lightgreen", "aquamarine", "springgreen", "green", "lightgreen", "aquamarine", "springgreen", "green", "lightgreen", "aquamarine", "springgreen", "green", "lightgreen", "aquamarine", "springgreen", "green", "lightgreen"]
 
-	if not use_rnn:
-		i = -1
-		for name in ["threshes", "k_ms", "asc_amps", "asc_rs", "asc_ks"]:
-			print(name)
-			i += 1
-			_, l = np.array(training_info[name]).shape
-			for j in range(l):
-				plt.plot(np.array(training_info[name])[:, j], color = colors[i], alpha = 0.5, label = name if j == 0 else "")
-		plt.legend()
-		plt.xlabel('epoch')
-		plt.ylabel('parameter')
-		plt.savefig("figures/" + base_name + "_parameters")
-		plt.close()
-	
-	if not use_rnn:
-		i = -1
-		for name in ["thresh_grads", "k_m_grads", "asc_amp_grads", "asc_r_grads"]:
-			print(name)
-			i += 1
-			_, l = np.array(training_info[name]).shape
-			for j in range(l):
-				plt.plot(np.array(training_info[name])[:, j], color = colors[i], alpha = 0.5, label = name if j == 0 else "")
-		plt.legend()
-		plt.xlabel('epoch')
-		plt.ylabel('parameter')
-		plt.savefig("figures/" + base_name + "_parameter_grads")
-		plt.close()
-	
-		# names = ["input_linear", "rec_linear", "output_linear"]
-		# i = 0
-		# for i in range(3):
-		# 	i += 1
-		# 	name = names[i]
-		# 	_, l = np.array(training_info["weights"][i]).shape
-		# 	for j in range(l):
-		# 		plt.plot(np.array(training_info["weights"][i])[:, j], color = colors[i], label = name if j == 0 else "")
-		# plt.legend()
-		# plt.xlabel('epoch')
-		# plt.ylabel('parameter')
-		# plt.savefig("figures/" + base_name + "_weights")
-		# plt.close()
-	
-	# torch.save(training_info["losses"], "traininfo/" + base_name_save + "_losses.pt")
-	with open("traininfo/" + base_name_save + ".pickle", 'wb') as handle:
-		pickle.dump(training_info, handle)
+        if not use_rnn:
+                i = -1
+                for name in ["threshes", "k_ms", "asc_amps", "asc_rs", "asc_ks"]:
+                        print(name)
+                        i += 1
+                        _, l = np.array(training_info[name]).shape
+                        for j in range(l):
+                                plt.plot(np.array(training_info[name])[:, j], color = colors[i], alpha = 0.5, label = name if j == 0 else "")
+                plt.legend()
+                plt.xlabel('epoch')
+                plt.ylabel('parameter')
+                plt.savefig("figures/" + base_name + "_parameters")
+                plt.close()
+        
+        if not use_rnn:
+                i = -1
+                for name in ["thresh_grads", "k_m_grads", "asc_amp_grads", "asc_r_grads"]:
+                        print(name)
+                        i += 1
+                        _, l = np.array(training_info[name]).shape
+                        for j in range(l):
+                                plt.plot(np.array(training_info[name])[:, j], color = colors[i], alpha = 0.5, label = name if j == 0 else "")
+                plt.legend()
+                plt.xlabel('epoch')
+                plt.ylabel('parameter')
+                plt.savefig("figures/" + base_name + "_parameter_grads")
+                plt.close()
+        
+                # names = ["input_linear", "rec_linear", "output_linear"]
+                # i = 0
+                # for i in range(3):
+                #       i += 1
+                #       name = names[i]
+                #       _, l = np.array(training_info["weights"][i]).shape
+                #       for j in range(l):
+                #               plt.plot(np.array(training_info["weights"][i])[:, j], color = colors[i], label = name if j == 0 else "")
+                # plt.legend()
+                # plt.xlabel('epoch')
+                # plt.ylabel('parameter')
+                # plt.savefig("figures/" + base_name + "_weights")
+                # plt.close()
+        
+        # torch.save(training_info["losses"], "traininfo/" + base_name_save + "_losses.pt")
+        with open("traininfo/" + base_name_save + ".pickle", 'wb') as handle:
+                pickle.dump(training_info, handle)
 
 if __name__ == '__main__':
-	main()
+        main()
