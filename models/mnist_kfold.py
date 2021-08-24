3e335bd9f838196f9e6e35a052ff49f311a1efc5
chloewin <wincnw@gmail.com>
2021-08-23 18:21:04 -0700

    new kfold results

--- models/mnist_kfold.py
+++ models/mnist_kfold.py
@@ -80,7 +80,7 @@ def main():
 
     k_folds = 5
     num_epochs = 10
-    regs = [1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1]
+    regs = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
     results = np.zeros((len(regs), k_folds))
 
     torch.manual_seed(42)
