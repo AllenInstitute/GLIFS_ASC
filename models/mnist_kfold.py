aca1c44df84ecd25d2a9d137db55e070c074bde2
chloewin <wincnw@gmail.com>
2021-08-27 16:35:07 -0700

    update kfold

--- models/mnist_kfold.py
+++ models/mnist_kfold.py
@@ -79,10 +79,10 @@ def main():
     num_ascs = args.numascs
 
     k_folds = 5
-    num_epochs = 15#0
+    num_epochs = 20#0
     # doppio/americano [1e-12, 1e-10, 1e-8, 1e-6, 1e-4]
     # others [1e-2, 1e0, 1e2, 1e4]
-    regs = [1e-12, 1e-9, 1e-6, 1e-3, 1e0, 1e3]#[1e-1, 1e0, 1e1, 1e2, 1e3]#[1e-6, 1e-5, 1e-4, 1e-3, 1e-2]
+    regs = [1e-15, 1e-12, 1e-9]#[1e-12, 1e-9, 1e-6, 1e-3, 1e0, 1e3]#[1e-1, 1e0, 1e1, 1e2, 1e3]#[1e-6, 1e-5, 1e-4, 1e-3, 1e-2]
     results = np.zeros((len(regs), k_folds))
 
     torch.manual_seed(42)
