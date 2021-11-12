11ca310bc7014ea6f34c6863445772a0a6323b58
chloewin <wincnw@gmail.com>
2021-11-11 20:21:44 -0800

    figures

--- models/results/network_traces.m
+++ models/results/network_traces.m
@@ -4,19 +4,16 @@ traces = xlsread(filename)
 linewidth = 2;
 fontsize=24;
 
-sim_time = 100;
+sim_time = 5;
 colors = ["#332288", "#44AA99", "#88CCEE", "#DDCC77", "#CC6677", "#AA4499", "#882255"];
 
 fig =figure;%('Position', [0,0,400,250]);
 fig.Renderer='Painters';
-subplot(5,1,1);
-for i = [1:size(traces,2)]
-    trace = traces(:, i)
+for i = [1:6]%[1:size(traces,2)]
+    subplot(3, 2, i)
+    trace = traces(:, randi(256))
     plot([0:0.05:sim_time-0.05], trace, 'LineWidth', linewidth);
     hold on
 end
-set(gca,'FontSize', fontsize);
-ylabel('input', 'FontName', 'helvetica', 'FontSize', fontsize);
 
 %legend('FontName', 'helvetica', 'FontSize', fontsize, 'Location', 'best')
-xlabel('time (ms)', 'FontName', 'helvetica', 'FontSize', fontsize);
